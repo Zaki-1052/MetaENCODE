c47d64c8cab8bd1aefa8daa6aa7dd46c4b3f3556
Zaki-1052 <134018102+Zaki-1052@users.noreply.github.com>
2026-01-15 16:28:10 -0800

    scaffolding

---
+++ src/visualization/__init__.py
@@ -0,0 +1,6 @@
+# src/visualization/__init__.py
+"""Visualization module for dimensionality reduction and plotting."""
+
+from .plots import DimensionalityReducer, PlotGenerator
+
+__all__ = ["DimensionalityReducer", "PlotGenerator"]
---
+++ tests/test_ml/__init__.py
@@ -0,0 +1,2 @@
+# tests/test_ml/__init__.py
+"""Tests for machine learning module."""
